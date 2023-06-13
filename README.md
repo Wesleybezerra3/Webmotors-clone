a6a87d4d7efa330221a33f0df4348f2e359740a0
Wesleybezerra3 <wesleybezerra770@gmail.com>
2023-06-13 09:33:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Webmotors-clone
+ 
---
+++ js/script.js
@@ -0,0 +1,110 @@
+$(document).ready(()=>{
+  let abrirNav =$('#abrirNav').click(()=>{
+    $('#container-navbar').css('display','block');
+  });
+
+ let fecharNav = $('#fecharNav').click(()=>{
+   $('#container-navbar').css('display','none');
+ });
+})
+
+$(document).ready(()=>{
+  $('#motos').click(()=>{
+    $('#motos').css({
+      'color':'#000000',
+        'border-bottom':'2px solid #f50707',
+          'font-size':'20px'
+         });
+
+         $('#search').prop('placeholder','Digite marca ou modelo da moto');
+
+       $('#carros').css({
+        'font-size':'18px',
+        'color':'#272424a8',
+        'border-bottom':'none'
+     });
+   });
+});
+
+   
+
+
+$(document).ready(()=>{
+  $('#carros').click(()=>{
+    $('#carros').css({'color':'#000000',
+      'border-bottom':'2px solid #f50707',
+        'font-size':'20px'
+});
+
+  $('#search').attr('placeholder','Digite marca ou modelo do carro');
+
+  $('#motos').css({'font-size':'18px',
+     'color':'#272424a8',
+       'border-bottom':'none'
+});
+  });
+    });
+
+
+
+// Cadastro
+
+
+
+
+$('#btnCadastro').click(()=>{
+  var email =$('#email').val()
+  var senha =$('#senha').val()
+
+
+    function validarEmail(email){
+      let padrao=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+      return padrao.test(email)
+  }
+  function validarSenha(senha){
+    let padrao = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    return padrao.test(senha)
+  }
+  if(validarEmail(email) && validarSenha){
+   alert('Sucesso')
+  }
+
+})
+$(document).ready(()=>{
+  let rotation=0
+ $('#summary1').click(()=>{
+  rotation+=180
+  $('#arrowDown1').css({'transform':`rotate(${rotation}deg)`})
+ })
+})
+$(document).ready(()=>{
+  let rotation=0
+ $('#summary2').click(()=>{
+  rotation+=180
+  $('#arrowDown2').css({'transform':`rotate(${rotation}deg)`})
+ })
+})
+$(document).ready(()=>{
+  let rotation=0
+ $('#summary3').click(()=>{
+  rotation+=180
+  $('#arrowDown3').css({'transform':`rotate(${rotation}deg)`})
+ })
+})
+$(document).ready(()=>{
+  let rotation=0
+ $('#summary4').click(()=>{
+  rotation+=180
+  $('#arrowDown').css({'transform':`rotate(${rotation}deg)`})
+ })
+})
+
+
+
+// $('#email').keyup(()=>{
+// $('#campoObrig').text($('#email').val())
+// })
+
+
+   
+ 
