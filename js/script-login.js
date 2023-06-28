e593e5043138f3fbc23f6edbf5d4ddb2a044772d
Wesleybezerra3 <wesleybezerra770@gmail.com>
2023-06-27 22:32:58 -0300

    Tela de cadastro concluída!!

--- js/script-login.js
+++ js/script-login.js
@@ -1,15 +1,18 @@
 // login
 $(document).ready(() => {
+
+  // sistema Validação
+
   $("#btnLogin").click(() => {
     var email = $("#email").val();
     var senha = $("#senha").val();
 
     function validarEmail(email) {
-      let padrao = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      let padrao = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/;
       return padrao.test(email);
     }
     function validarSenha(senha) {
-      let padrao =/^(.{8,})$/
+      let padrao = /^(.{8,})$/;
 
       return padrao.test(senha);
     }
@@ -19,52 +22,79 @@ $(document).ready(() => {
       alert("[ERROR]");
     }
   });
-});
 
-$(document).ready(() => {
+  //  sistema Verificação
+
   $("#email").keyup(() => {
     let email = $("#email").val();
     function verificarEmail(email) {
-      let padrao = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-      return padrao.test(email)
+      let padrao = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/;
+      return padrao.test(email);
     }
     if (verificarEmail(email) == false) {
       $("#campoObrigEmail").css({
-        display: "block"
+        display: "block",
       });
       $("#email").css({
-        'border':'2px solid blueviolet'
-      })
+        border: "2px solid blueviolet",
+      });
     } else {
       $("#campoObrigEmail").css({
-        'display': "none"
+        display: "none",
       });
       $("#email").css({
-        'border':'1px solid rgba(46, 46, 46, 0.26)'
-      })
+        border: "1px solid rgba(46, 46, 46, 0.26)",
+      });
     }
   });
   $("#senha").keyup(() => {
     let senha = $("#senha").val();
     function verificarSenha(senha) {
-      let padrao =/^(.{8,})$/
-
-      return padrao.test(senha)
+      let padrao = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/; //Pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais:
+      return padrao.test(senha);
     }
     if (verificarSenha(senha) == false) {
       $("#campoObrigSenha").css({
-        "display": "block"
+        display: "block",
       });
       $("#senha").css({
-        'border':'2px solid blueviolet'
-      })
+        border: "2px solid blueviolet",
+      });
     } else {
       $("#campoObrigSenha").css({
-        "display": "none"
+        display: "none",
       });
       $("#senha").css({
-        'border':'1px solid rgba(46, 46, 46, 0.26)'
-      })
+        border: "1px solid rgba(46, 46, 46, 0.26)",
+      });
+    }
+  });
+  $("#name, #emailRegister").keyup(() => {
+    let nome = $("#name").val();
+    let email = $("#emailRegister").val();
+    const criarConta = $("#btnRegister");
+
+    function verificarEmail(email) {
+      let padrao = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/;
+      return padrao.test(email);
+    }
+    function verificarNome(nome) {
+      let padrao = /^[a-zA-Z\s'-]+$/;
+      return padrao.test(nome);
+    }
+    if (verificarNome(nome) === false || verificarEmail(email) === false) {
+      criarConta.attr("disabled", true);
+      $("#btnRegister").css({
+        "background-color": "#e44663e5",
+      });
+    } else {
+      criarConta.removeClass("link-desativado");
+      criarConta.addClass("link-ativado");
+      criarConta.attr("disabled", false);
+      $("#btnRegister").css({
+        "background-color": "",
+      });
     }
   });
 });
+
